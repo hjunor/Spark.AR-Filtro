074c0339f376d723bbec60dc22fc0eb1227f1047
Heberth Junor Teixeira Aguiar <hjunor@gmail.com>
2020-02-07 21:37:28 -0300

    Estuturando

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-/**
- * (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
- */
-
-//==============================================================================
-// Welcome to scripting in Spark AR Studio! Helpful links:
-//
-// Scripting Basics - https://fb.me/spark-scripting-basics
-// Reactive Programming - https://fb.me/spark-reactive-programming
-// Scripting Object Reference - https://fb.me/spark-scripting-reference
-// Changelogs - https://fb.me/spark-changelog
-//==============================================================================
 
 // How to load in modules
 const Scene = require('Scene');
